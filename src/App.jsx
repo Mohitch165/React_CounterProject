584e452e4c16666c18f656361c01bf43a08824dc
Mohit Chouhan <info.mohit321@gmail.com>
2025-02-11 19:04:49 +0000

    Add functionality changes to create the Counter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+let userName = "Mohit"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Counter Project</h1>
+      <h2>Current Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>{" "}
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <h3>Footer: {count}</h3>
+      <h4>Hello my name is {userName}, This is my first React Project.</h4>
     </>
   )
 }
